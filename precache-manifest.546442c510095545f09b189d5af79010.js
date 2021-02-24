c0386d773e74dfa4a617cc6d445953759898b6c6
ranarupesh <rupeshrana288@gamil.com>
2021-02-24 12:46:00 +0530

    Updates

---
+++ precache-manifest.546442c510095545f09b189d5af79010.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4940710cf7212d56d5c793a16c3dc08f",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "04422f97dd7005770065",
+    "url": "/covid19/static/css/main.488b1944.chunk.css"
+  },
+  {
+    "revision": "dbafa8feedc64c36bdaa",
+    "url": "/covid19/static/js/2.22644c91.chunk.js"
+  },
+  {
+    "revision": "d6bdd298aac5384fb5b7c2e2efb6109b",
+    "url": "/covid19/static/js/2.22644c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04422f97dd7005770065",
+    "url": "/covid19/static/js/main.21335987.chunk.js"
+  },
+  {
+    "revision": "34491f16cdd8cac9985a",
+    "url": "/covid19/static/js/runtime-main.fa1deedf.js"
+  },
+  {
+    "revision": "1c8895dd79172b2f85043bf51fd5c7fa",
+    "url": "/covid19/static/media/01.1c8895dd.svg"
+  },
+  {
+    "revision": "4e94bdfbeca8646b61a50920291ffaad",
+    "url": "/covid19/static/media/02.4e94bdfb.svg"
+  },
+  {
+    "revision": "046acf9af57fab1131779a6afc365f0c",
+    "url": "/covid19/static/media/03.046acf9a.svg"
+  },
+  {
+    "revision": "577bf8834560303fc0ebc011d8334d04",
+    "url": "/covid19/static/media/04.577bf883.svg"
+  },
+  {
+    "revision": "6a1ae40a1db62466bd6bbda2cd4e581a",
+    "url": "/covid19/static/media/05.6a1ae40a.svg"
+  },
+  {
+    "revision": "205f578ca91d90b22afa873d2faf3b5a",
+    "url": "/covid19/static/media/06.205f578c.svg"
+  }
+]);
